6c6bdb5f9dbc9fa79f14fb2714a4a9b7f11c9fd2
Aditya Vyavahare <vvaditya23@icloud.com>
2023-09-29 22:57:17 +0530

    Completed an independent logic for generating a strong password of length 12

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 #Password Generator Project
 import random
 
+#max_length = 12
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
 password = []
 password_str = ""
 
-for char in range(0, nr_letters):
+for char in range(0, 5):
   random_letter = random.choice(letters)
   password.append(random_letter)
   #print(random_letter)
 
-for num in range(0, nr_symbols):
+for num in range(0, 5):
   random_number = random.choice(numbers)
   password.append(random_number)
 
-for sym in range(0, nr_symbols):
+for sym in range(0, 5):
   random_symbol = random.choice(symbols)
   password.append(random_symbol)
 
